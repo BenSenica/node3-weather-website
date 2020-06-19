f1ff37d179b3b9ccc29195f1c77786c20db96089
BenSenica <senicaben@gmail.com>
2020-06-19 12:03:56 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require ('request')
+const { isRegExp } = require('util')
+
+const forecast = (lat, long, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7a71b08200cf579dbbbca8ab4b331b34&query=' + lat + ',' + long + '&units=m'
+
+    request({url, json: true}, (error, {body}) => {
+        if (error){
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.error){
+            callback('Unable to find location. Please try again.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees.')
+        }
+    })
+}
+
+module.exports = forecast
